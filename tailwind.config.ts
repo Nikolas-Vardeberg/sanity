c973dd8ddf4c22803926b69d43b6cd7e94b24567
Nikolas-Vardeberg <post@nikolas.no>
2024-05-14 23:46:54 +0200

    css: new tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,13 +7,47 @@ const config: Config = {
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    container: {
+      center: true,
+      padding: {
+        DEFAULT: "1rem",
+        sm: "2rem",
+        lg: "3rem",
+        xl: "7rem",
+        "2xl": "8rem",
+      }
+    },
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+    backgroundImage: {},
+     colors: {
+      primary: {
+        "50": "#f6fee7",
+        "100": "#eafccb",
+        "200": "#d7f99d",
+        "300": "#bef264",
+        "400": "#a5e635",
+        "500": "#89cc16",
+        "600": "#6ca30d",
+        "700": "#547c0f",
+        "800": "#456212",
+        "900": "#3c5314",
+        "950": "#1f2e05",
+      },
+      secondary: {
+        "50": "#ecfdf9",
+        "100": "#d1faf0",
+        "200": "#a7f3el",
+        "300": "#6ee7ca",
+        "400": "#34d3ad",
+        "500": "#10b991",
+        "600": "#059673",
+        "700": "#04785c",
+        "800": "#065f4a",
+        "900": "#064e3d",
+        "950": "#022c22",
       },
     },
+  },
   },
   plugins: [],
 };
