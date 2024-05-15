0e46792c56e80131cefda30909c2841133bca951
niklusrook <post@nikolas.no>
2024-05-15 18:50:52 +0200

    feat: hero section (without sanity)

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,18 @@
 import Logo from "@/components/elements/icons/Logo";
 import Header from "@/components/layout/Header";
-import { Hero } from "@/components/sections/hero/page";
+import About from "@/components/sections/about-us/About";
+import Features from "@/components/sections/features/Features";
+import { Hero } from "@/components/sections/hero/Hero";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <main className="no-scrollbar">
       <Hero />
+      <div className="p-4">
+        <Features />
+      </div>
+      <About />
     </main>
   );
 }
---
+++ components/sections/features/Features.tsx
@@ -0,0 +1,69 @@
+import Ev from "@/components/elements/icons/Ev"
+
+const Features = () => {
+  return (
+    <section>
+        <div className="container">
+            <div className="flex items-center gap-2 border-b border-neutral-60  pb-4">
+                <span className=""/>
+                <span>Solutions</span>
+            </div>
+
+            <div>
+                <div>
+                    <h2 className="text-section leading-none">Key to clean future</h2>
+                </div>
+
+                <div className="grid md:grid-cols-2 gap-8 xl:grid-cols-4">
+                    <div className="flex flex-col gap-6 items-start">
+                        <Ev />
+                        <div>
+                            <h3 className="text-title leading-none mb-4">Ev charging</h3>
+                            <p className="text-neutral-800">
+                                Evs use electricty as a power source, which can be generated from renewable energy sources. Our solutions help reducing greenhouse gas emissions in the transport sections.
+                            </p>
+                        </div>
+                        <button className="text-primary-950">Read More</button>
+                    </div>
+
+                    <div className="flex flex-col gap-6 items-start">
+                        <Ev />
+                        <div>
+                            <h3 className="text-title leading-none mb-4">Ev charging</h3>
+                            <p className="text-neutral-800">
+                                Evs use electricty as a power source, which can be generated from renewable energy sources. Our solutions help reducing greenhouse gas emissions in the transport sections.
+                            </p>
+                        </div>
+                        <button className="text-primary-950">Read More</button>
+                    </div>
+
+                    <div className="flex flex-col gap-6 items-start">
+                        <Ev />
+                        <div>
+                            <h3 className="text-title leading-none mb-4">Ev charging</h3>
+                            <p className="text-neutral-800">
+                                Evs use electricty as a power source, which can be generated from renewable energy sources. Our solutions help reducing greenhouse gas emissions in the transport sections.
+                            </p>
+                        </div>
+                        <button className="text-primary-950">Read More</button>
+                    </div>
+
+                    <div className="flex flex-col gap-6 items-start">
+                        <Ev />
+                        <div>
+                            <h3 className="text-title leading-none mb-4">Ev charging</h3>
+                            <p className="text-neutral-800">
+                                Evs use electricty as a power source, which can be generated from renewable energy sources. Our solutions help reducing greenhouse gas emissions in the transport sections.
+                            </p>
+                        </div>
+                        <button className="text-primary-950">Read More</button>
+                    </div>
+                    
+                </div>
+            </div>
+        </div>
+    </section>
+  )
+}
+
+export default Features
